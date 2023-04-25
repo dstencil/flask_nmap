0e3581a5980940366620204d05814041230bb61f
Dylan Stencil <35854396+dstencil@users.noreply.github.com>
2023-04-25 20:26:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask import Flask, render_template, request, jsonify, redirect, url_for, make_response
 import nmap
 import networkx as nx
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import json
+import sqlite3
+import pdfkit
+import csv
+from io import BytesIO
+
+
 
 app = Flask(__name__)
 
@@ -73,6 +81,41 @@ def visualize_network_topology(G):
     plt.axis('off')
     plt.savefig('static/network_topology.png', format='png')  # Save the plot as an image in 'static' folder
     plt.close()
+# Step 4: Create devices table in SQLite database
+def create_devices_table(conn):
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS devices
+                      (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       ip TEXT NOT NULL,
+                       hostname TEXT,
+                       mac TEXT,
+                       ipv4 TEXT,
+                       ipv6 TEXT,
+                       vendor TEXT)''')
+    conn.commit()
+
+# Step 5: Connect to SQLite database
+def connect_to_database():
+    conn = sqlite3.connect('devices.db')
+    return conn
+
+# Step 6: Save devices into SQLite database
+def save_devices_to_database(conn, devices):
+    cursor = conn.cursor()
+    for device in devices:
+        cursor.execute('''INSERT INTO devices (ip, hostname, mac, ipv4, ipv6, vendor)
+                          VALUES (?, ?, ?, ?, ?, ?)''',
+                       (device['ip'], device['hostname'], device['mac'], device['ipv4'], device['ipv6'], device['vendor']))
+    conn.commit()
+
+def save_devices_to_csv(devices, filename):
+    with open(filename, mode='w') as csv_file:
+        fieldnames = ['ip', 'hostname', 'mac', 'ipv4', 'ipv6', 'vendor']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for device in devices:
+            writer.writerow(device)
 
 @app.route('/')
 def index():
@@ -88,6 +131,9 @@ def map():
         # Perform network scanning and retrieve list of devices
         devices = perform_network_discovery(network_range)
         
+        # Save devices to CSV file
+        save_devices_to_csv(devices, 'static/devices.csv')
+        
         # Build network graph
         G = build_network_graph(devices)
         
@@ -96,6 +142,13 @@ def map():
         
         # Return the list of devices and network topology image in the response
         return render_template('map.html', devices=devices, topology_image='static/network_topology.png')
+@app.route('/download')
+def download_file():
+	#path = "html2pdf.pdf"
+	#path = "info.xlsx"
+	path = "static/network_topology.png"
+	#path = "sample.txt"
+	return send_file(path, as_attachment=True)
 
 
 if __name__ == '__main__':
